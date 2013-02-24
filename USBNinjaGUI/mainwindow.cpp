004ba09fa718a726d2e701f384b8ff011a2ee436
Grant Foudree <fred.bobbert@mail.com>
2013-02-24 01:10:23 -0600

    Added CSS style for GUI.

--- USBNinjaGUI/mainwindow.cpp
+++ USBNinjaGUI/mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QStringList columnTitles;
+    columnTitles << "ID" << "Action" << "Date" << "User" << "Label" << "Manufacturer" << "Size";
+    ui->treeWidget->setHeaderLabels(columnTitles);
+    ui->treeWidget->resizeColumnToContents(0);
 }
 
 MainWindow::~MainWindow()
