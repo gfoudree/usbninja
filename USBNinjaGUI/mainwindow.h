43b48ac3d3d9919977d0be65f4f91c029fb76fae
Grant Foudree <grant907@gmail.com>
2013-06-30 03:42:02 -0500

    Fixed a few files that were missing licenses.

--- USBNinjaGUI/mainwindow.h
+++ USBNinjaGUI/mainwindow.h
@@ -13,7 +13,18 @@
  
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+/*
+ * Copyright (C) by Grant Foudree <grant907@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
 #include <QMainWindow>
 #include <QFileDialog>
 #include <windows.h>
